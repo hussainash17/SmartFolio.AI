b9ed8f26cb67c7d4832349bd06a03885d4d4614b
Md Ashraf Hussain <ashraf@dohatec.com.bd>
2025-08-14 13:00:53 +0600

    updated the table of cash balance

--- backend/app/alembic/versions/2025_09_01_0001_add_cash_balance_to_portfolio.py
+++ backend/app/alembic/versions/2025_09_01_0001_add_cash_balance_to_portfolio.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'add_cash_balance_to_portfolio'
-down_revision = 'ea5530c74046'
+down_revision = '2024_08_14_1200'
 branch_labels = None
 depends_on = None
 
